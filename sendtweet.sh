ff3323dc7db88b9dea74c00cd4a80ae89e7b7759
ononoy <30824451+ononoy@users.noreply.github.com>
2017-11-11 18:55:00 +0900

    Delete sendtweet.sh

--- sendtweet.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-D=`date +%F-%H-%M`
-echo $D >> /home/ubuntu/text_writer/src/tweeted.log
-/usr/bin/python3 /home/ubuntu/text_writer/src/maketweet.py >> /home/ubuntu/text_writer/src/tweeted.log
